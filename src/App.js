eb782f927d3b2fbc9423a4b8c8c50d0244894f33
gtole342 <gtole342@gmail.com>
2019-10-03 11:13:51 -0700

    Inital commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{ useState } from 'react'
+import VectorTile from "@mapbox/vector-tile";
+import MapGL, {GeolocateControl } from 'react-map-gl'
+import 'mapbox-gl/dist/mapbox-gl.css'
 
-function App() {
+const TOKEN="pk.eyJ1IjoiZ3RvbGUiLCJhIjoiY2p6b2Y4cGw4MDJlYTNtbm1zc3dpd3BnciJ9.ZR0TDB4dlK-kw5DHO4qM1w";
+
+const App = () => {
+
+  const [viewport, setViewPort ] = useState({
+    width: 400,
+    height: 400,
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 15
+  })
+
+  const _onViewportChange = viewport => setViewPort({...viewport, transitionDuration: 3000 })
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <MapGL
+        {...viewport}
+        mapboxApiAccessToken={TOKEN}
+        mapStyle="mapbox://styles/mapbox/outdoors-v11"
+        onViewportChange={_onViewportChange}
+      >
+      </MapGL>
+  )
 }
 
 export default App;
