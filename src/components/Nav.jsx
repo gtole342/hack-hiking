9ae91fe894ab7597188ccc9be4935774c211c34e
michelleurrea <michelleurrea@gmail.com>
2019-10-04 09:09:37 -0700

    Everything completed on day 1

---
+++ src/components/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavMaterialUI from '../components/NavMaterialUI'
+
+
+const Nav = () => {
+    return (
+        <div>
+            <NavMaterialUI />
+        </div>
+    )
+}
+
+export default Nav;
